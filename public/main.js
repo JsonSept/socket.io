d0ea398ce12ca062d9c5357f0eaa4a436a2a2539
JsonSept <jasonseptember.za@gmail.com>
2024-03-07 23:42:48 +0200

    finished tutorial

--- public/main.js
+++ public/main.js
@@ -6,6 +6,8 @@ const nameInput = document.getElementById('name-input')
 const messageForm = document.getElementById('message-form')
 const messageInput = document.getElementById('message-input')
 
+const messageTone = new Audio('./message-tone.mp3')
+
 messageForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     sendMessage()
@@ -30,10 +32,12 @@ function sendMessage() {
 
 socket.on('chat-message', (data) => {
     // console.log(data)
+    messageTone.play()
     addMessageToUI(false,data)
 })
 
 function addMessageToUI(isOwnMessage, data) {
+    clearFeedback()
  const element =   `
     <li class="${isOwnMessage ? "message-right" : "message-left"}">
                 <p class="message">
@@ -47,5 +51,39 @@ function addMessageToUI(isOwnMessage, data) {
 }
 
 function scrollToBottom() {
-    messageContainer.scrollTo(0, messagecontainer.scrollHeight)
+    messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
+
+messageInput.addEventListener('focus', (e) => {
+    socket.emit('feedback', {
+        feedback: `@ ${nameInput.value} is typing a message`
+    })
+})
+
+messageInput.addEventListener('keypress', (e) => {
+    socket.emit('feedback', {
+        feedback: `@ ${nameInput.value} is typing a message`
+    })
+})
+messageInput.addEventListener('blur', (e) => {
+    socket.emit('feedback',{
+        feedback: '',
+    })
+})
+
+socket.on('feedback', (data)=> {
+    clearFeedback()
+    const element = `
+    <li class="message-feedback">
+        <p class="feedback" id="feedback">${data.feedback}</p>
+    </li>
+    `
+    messageContainer.innerHTML += element
+})
+
+function clearFeedback() {
+    document.querySelectorAll('li.message-feedback').forEach(element => {
+        element.parentNode.removeChild(element)
+
+    })
 }
